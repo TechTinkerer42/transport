2dff228696aa6877afc286949faafda3258531e7
Michael Fortin <michael@m410.us>
2015-12-09 11:28:16 -0500

    removed dependencies from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 var tsc = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 var tslint = require('gulp-tslint');
 var config = require('./gulp.config')();
 var tsProject = tsc.createProject('./src/tsconfig.json');
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var superstatic = require('superstatic');
 var del = require('del');
 
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-	
+
 });
 
 gulp.task('html', function() {
@@ -51,34 +49,34 @@ gulp.task('compile-ts', function() {
 	   config.allTs,
 	   config.typings
      ];
-	
+
 	return gulp
 		.src(sourceTsFiles)
 		.pipe(tsc(tsProject))
 		.pipe(gulp.dest(config.tsOutputPath));
-		
+
 	return tsResult;
 });
 
 gulp.task('build', ['ts-lint', 'compile-ts', 'copy-resources']);
 
 gulp.task('serve', ['build'], function() {
-	
+
 	gulp.watch([config.allTs, '**/*.html'], ['build']);
-	
+
 	browserSync({
 		port: 3000,
     files: ['src/index.html', '**/*.js', 'css/*'],
     injectChanges: true,
     logFileChanges: false,
-    logLevel: 'silent',    
+    logLevel: 'silent',
     notify: true,
     reloadDelay: 0,
     server: {
       baseDir: './scripts/build'
     }
 	});
-	
+
 });
 
 gulp.task('default', ['serve']);
