0d618a984a84f3c88e426352634846400ff8d558
Michael Fortin <michael@m410.us>
2015-12-11 20:10:32 -0500

    not working yet

---
+++ src/app/agent/agent.ts
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component( {
+	selector: 'agent'
+})
+@View({
+	templateUrl: 'index.html'
+})
+
+export class Agent {
+}
---
+++ src/app/home/home.ts
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component( {
+	selector: 'home'
+})
+@View({
+	templateUrl: 'index.html'
+})
+
+export class Home {
+}
